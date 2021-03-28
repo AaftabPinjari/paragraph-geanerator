668774a7d4a657af92fee77b75562b4eb0837668
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-03-28 09:59:52 +0530

    done generating paragraphs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
 function App() {
+  const [count,setCount]= useState(0);
+  const [text,setText]=useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count<=0){
+      amount =0
+    }
+    if(count>8){
+      amount=8
+    }
+    setText(data.slice(0,amount));
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <section className="section-center">
+    <h3>Here is my Paragraph Generator</h3>
+    <form className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="amount">
+        Paragraphs:
+      </label>
+      <input type="number" name="amount" id="amount" vlaue={count}
+      onChange={(e)=> setCount(e.target.value)}
+      />
+      <button type="submit" className="btn">generate</button>
+    </form>
+    <article className="lorem-text">
+    {text.map((item,index)=>{
+      return <p key={index} >{item}</p>
+    })}
+    </article>
+  
+  </section>
+    )
 }
 
 export default App;
